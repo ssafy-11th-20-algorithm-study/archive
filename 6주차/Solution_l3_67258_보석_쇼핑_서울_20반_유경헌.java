958124dbc79d708573c4389c42108574fea320a4
yootal <khyoo96@naver.com>
2024-04-07 16:15:00 +0900

    0407

--- 6주차/Solution_l3_67258_보석_쇼핑_서울_20반_유경헌.java
+++ 6주차/Solution_l3_67258_보석_쇼핑_서울_20반_유경헌.java
@@ -3,49 +3,47 @@ package com.algorithm.study.eclipse;
 import java.util.*;
 
 class Solution_l3_67258_보석_쇼핑_서울_20반_유경헌 {
-	public static int[] solution(String[] gems) {
-		int n = gems.length;
-		int vis[] = new int[n];
-		HashMap<String, Integer> map = new HashMap<>();
-
-		int idx = 0;
-		for (int i = 0; i < n; i++) {
-			if (!map.containsKey(gems[i])) {
-				map.put(gems[i], idx++);
-			}
-		}
-
-		// 투 포인터
-		int range = Integer.MAX_VALUE;
-		int ans[] = new int[2];
-		int limit = map.size();
-		int size = 0;
-		int end = 0;
-		for (int start = 0; start < n; start++) {
-			while (end != n && size < limit) {
-				int curEd = map.get(gems[end]);
-				if (vis[curEd] == 0) {
-					size++;
-				}
-				vis[curEd]++;
-				end++;
-			}
-			if (size == limit) {
-				int diff = end - start;
-				if (range > diff) {
-					range = diff;
-					ans[0] = start + 1;
-					ans[1] = end;
-				}
-			}
-			int curSt = map.get(gems[start]);
-			if (vis[curSt] > 0) {
-				vis[curSt]--;
-				if (vis[curSt] == 0)
-					size--;
-			}
-
-		}
-		return ans;
-	}
+    public static int[] solution(String[] gems) {
+        int n = gems.length;
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        int idx = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(!map.containsKey(gems[i])){
+                map.put(gems[i],idx++);
+            }
+        }
+        
+        int range = Integer.MAX_VALUE;
+        int ans[] = new int[2];
+        int limit = map.size();
+        int vis[] = new int[limit];
+        int size = 0;
+        int end = 0;
+        for(int start = 0 ; start < n ; start++){
+            while(end != n && size < limit){
+                int curEd = map.get(gems[end]);
+                if(vis[curEd] == 0){
+                    size++;
+                }
+                vis[curEd]++;
+                end++;
+            }
+            if(size == limit){
+                int diff = end - start;
+                if(range > diff){
+                    range = diff;
+                    ans[0] = start+1;
+                    ans[1] = end;
+                }
+            }
+            int curSt = map.get(gems[start]);
+            if(vis[curSt] > 0){
+                vis[curSt]--;
+                if(vis[curSt] == 0) size--;
+            }
+            
+        }
+        return ans;
+    }
 }
