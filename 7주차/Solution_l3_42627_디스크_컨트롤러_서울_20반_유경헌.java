55da234704b8f7c7563379f67de84951fd288807
yootal <92325898+yootal@users.noreply.github.com>
2024-04-25 21:09:38 +0900

    0425

--- 7주차/Solution_l3_42627_디스크_컨트롤러_서울_20반_유경헌.java
+++ 7주차/Solution_l3_42627_디스크_컨트롤러_서울_20반_유경헌.java
@@ -6,13 +6,16 @@ class Solution_l3_42627_디스크_컨트롤러_서울_20반_유경헌 {
 	public int solution(int[][] jobs) {
 		PriorityQueue<int[]> inPq = new PriorityQueue<>(
 				(o1, o2) -> o1[0] == o2[0] ? Integer.compare(o1[1], o2[1]) : Integer.compare(o1[0], o2[0]));
-		PriorityQueue<int[]> runPq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[1], o2[1]));
+		PriorityQueue<int[]> runPq = new PriorityQueue<>(
+				(o1, o2) -> o1[1] == o2[1] ? Integer.compare(o1[0], o2[0]) : Integer.compare(o1[1], o2[1]));
+		int minIn = Integer.MAX_VALUE;
 		for (int i = 0; i < jobs.length; i++) {
+			minIn = Math.min(minIn, jobs[i][0]);
 			inPq.offer(new int[] { jobs[i][0], jobs[i][1] });
 		}
 		int answer = 0;
 		int cnt = 0;
-		int wait = 0;
+		int wait = minIn;
 		while (cnt < jobs.length) {
 			while (!inPq.isEmpty() && inPq.peek()[0] <= wait) {
 				runPq.offer(inPq.poll());
@@ -24,6 +27,7 @@ class Solution_l3_42627_디스크_컨트롤러_서울_20반_유경헌 {
 				cnt++;
 			} else {
 				int cur[] = inPq.poll();
+				wait = cur[0];
 				answer += cur[1] + wait - cur[0];
 				wait += cur[1];
 				cnt++;
