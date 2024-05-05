42cf23a12c165a59867a26533947be2e9d18a600
yootal <khyoo96@naver.com>
2024-05-05 14:03:34 +0900

    0505 어린이날!

---
+++ 9주차/Solution_l2_131130_혼자_놀기의_달인_서울_20반_유경헌.java
@@ -0,0 +1,36 @@
+package com.algorithm.study.eclipse;
+
+import java.util.*;
+
+class Solution_l2_131130_혼자_놀기의_달인_서울_20반_유경헌 {
+	static int c[];
+	static boolean vis[];
+
+	public int solution(int[] cards) {
+		c = cards;
+		vis = new boolean[cards.length];
+		PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+		for (int i = 0; i < cards.length; i++) {
+			if (!vis[i]) {
+				pq.offer(select(i));
+			}
+		}
+		int answer = 1;
+		for (int i = 0; i < 2; i++) {
+			int o = pq.poll();
+			if (o == cards.length) {
+				answer = 0;
+				break;
+			}
+			answer *= o;
+		}
+		return answer;
+	}
+
+	static int select(int x) {
+		if (vis[x])
+			return 0;
+		vis[x] = true;
+		return 1 + select(c[x] - 1);
+	}
+}
